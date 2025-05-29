3e0756b6d8fc1fd603355f74749ea02394063f9a
Khoi <minhkhoigacon@gmail.com>
2025-05-29 14:20:37 +0700

    test

---
+++ src/controllers/MonHoc/MHDeleteController.java
@@ -0,0 +1,12 @@
+package controllers.MonHoc;
+
+import javafx.event.ActionEvent;
+
+public class MHDeleteController {
+    public void handleXoa(ActionEvent actionEvent) {
+
+    }
+
+    public void handleHuy(ActionEvent actionEvent) {
+    }
+}
